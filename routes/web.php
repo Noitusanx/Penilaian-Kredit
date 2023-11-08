8499cb6bd31fa243ad59fe7ace4bab5037060070
IchwanulFata <ichwanul21@mhs.unsyiah.ac.id>
2023-11-09 00:39:49 +0700

    feat: tampilan halaman profil user

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get("/page", [PenilaianKreditController::class,"index"]);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user/profil');
 });
 
