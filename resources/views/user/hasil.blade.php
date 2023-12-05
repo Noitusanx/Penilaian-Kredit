92591f1a2948cabee934b74dd77675e6e7920605
Noitusanx <habilnasutionx@gmail.com>
2023-12-05 23:16:07 +0700

    fix: navbar download pengajuan diubah menjadi button

--- resources/views/user/hasil.blade.php
+++ resources/views/user/hasil.blade.php
@@ -13,9 +13,16 @@ Hasil Keputusan
 <div class="p-[100px] flex justify-center">
   <div class="w-[1000px] h-[500px] rounded-3xl flex items-center justify-center">
     @if ($status == "terima")
-    <div class="p-[100px] flex justify-center">
-      <div class="w-[1000px] h-[500px] bg-[#68A46C] rounded-3xl flex items-center justify-center">
-        <h2 class="font-bold text-5xl text-white text-center">SELAMAT! Pinjaman Anda Disetujui</h2>
+    <div class="p-[100px] flex flex-col items-center">
+      <div class="w-[1000px] h-[500px] bg-[#68A46C] rounded-3xl flex flex-col items-center justify-center relative">
+        <h2 class="font-bold text-5xl text-white text-center my-8">SELAMAT! Pinjaman Anda Disetujui</h2>
+        <button type="button"
+          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm text-center px-5 py-3 mb-5">
+          <a href="{{ url("admin/$pengajuan->id/detailpengajuan/pdf") }}">Download Berkas Pengajuan</a>
+        </button>
+        <p class=" text-center mt-4 mx-6">Silahkan mengunjungi kantor cabang terdekat untuk melanjutkan proses
+          verifikasi pengajuan pinjaman dengan membawa berkas pengajuan dan berkas penunjang lainnya
+        </p>
       </div>
     </div>
     @else
